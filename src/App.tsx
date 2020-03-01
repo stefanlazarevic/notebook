85f427f0e45ebb8f250ccef1475e6414b31e6e59
Stefan <stefanlazarevic.contact@gmail.com>
2020-03-01 18:32:49 +0100

    Added Application Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import "./App.css";
 
 import NoteEditorContainer from "./components/NoteEditor/NoteEditorContainer";
+import AppHeader from "./components/AppHeader/AppHeader";
 
 function App() {
   return (
     <div className="App">
+      <AppHeader />
       {/* Test component */}
       <NoteEditorContainer />
     </div>
---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./AppHeader.css";
+
+export default function AppHeader(props: any) {
+  return (
+    <header id="AppHeader" className="AppHeader">
+      <h1 className="title">Notebook</h1>
+    </header>
+  );
+}
