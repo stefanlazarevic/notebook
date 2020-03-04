4a0ce10464a0cd9123d9ae7eed8fbaa6e23be33a
Stefan <stefanlazarevic.contact@gmail.com>
2020-03-04 14:07:01 +0100

    Updates

---
+++ precache-manifest.976b0c85cd5bf43ad2aad560cc29d696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c25b4713d2505289453a9df43f8311e",
+    "url": "/notebook/index.html"
+  },
+  {
+    "revision": "72d5bf5cca9ea1bb3d9b",
+    "url": "/notebook/static/css/main.ad732820.chunk.css"
+  },
+  {
+    "revision": "f6fb6ac7dba25fff9e28",
+    "url": "/notebook/static/js/2.2983ecce.chunk.js"
+  },
+  {
+    "revision": "9b292ac27dc4155e41468b21d153d251",
+    "url": "/notebook/static/js/2.2983ecce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d5bf5cca9ea1bb3d9b",
+    "url": "/notebook/static/js/main.55c06b84.chunk.js"
+  },
+  {
+    "revision": "f4cb0bedee2faa4278ec",
+    "url": "/notebook/static/js/runtime-main.737af31e.js"
+  }
+]);
