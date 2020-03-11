6797c6e0301677bae27ce2b7143ab274ce09c01c
Stefan <stefanlazarevic.contact@gmail.com>
2020-03-12 00:50:01 +0100

    Migrated to the grid css layout.

--- src/components/Notes/Notes.tsx
+++ src/components/Notes/Notes.tsx
@@ -9,24 +9,11 @@ import NoteContainer from "./components/Note";
 export default function Notes(props: any) {
   return (
     <div className="Notes">
-      {utils.array
-        .chunk(props.children, 7)
-        .map((chunk: any[], rowIndex: number) => {
-          return (
-            <div className="NotesRow" key={rowIndex}>
-              {chunk.map((id: string, index: number) => {
-                return (
-                  <NoteContainer
-                    key={id}
-                    id={id}
-                    index={index}
-                    tabIndex={rowIndex + index}
-                  />
-                );
-              })}
-            </div>
-          );
-        })}
+      {utils.array.map((id: any[], index: number) => {
+        return (
+          <NoteContainer key={id} id={id} index={index} tabIndex={index} />
+        );
+      }, props.children)}
     </div>
   );
 }
