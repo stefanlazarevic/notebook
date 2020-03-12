d53c0f5312c0ee7259e9219c90a672f2bbc1cfdf
Stefan <stefanlazarevic.contact@gmail.com>
2020-03-12 11:44:42 +0100

    Added initial property descriptions.

--- src/components/Notes/components/Group/GroupProps.ts
+++ src/components/Notes/components/Group/GroupProps.ts
@@ -1,12 +1,34 @@
 import { NoteGroupID } from "../../../../redux/notes/groups/types";
 
 export interface NoteGroupProps {
+  /**
+   * Group unique identifier.
+   */
   id: NoteGroupID;
+
+  /**
+   * Position of the group in the parent group array.
+   */
   index: number;
 
+  /**
+   * Focus order number.
+   */
   tabIndex?: number;
+
+  /**
+   * Name of the group.
+   */
   title?: string;
+
+  /**
+   * Group id which contains this group.
+   */
   parent: NoteGroupID;
+
+  /**
+   * Currently opened group parent group id.
+   */
   groupParent?: NoteGroupID;
 
   moveToGroup?: (targetGroupID: NoteGroupID, children: string[]) => void;
