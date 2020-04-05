68952165ce986d10db506cce4efda1d2541e4ac3
Stefan Lazarevic <stefanlazarevic.contact@gmail.com>
2020-04-05 22:10:44 +0200

    Fixed back button.

--- src/components/Navigator/components/BackButton/BackButton.tsx
+++ src/components/Navigator/components/BackButton/BackButton.tsx
@@ -6,23 +6,30 @@ import { AppState } from "../../../../redux/types";
 import { openGroup } from "../../../../redux/notes/actions";
 import utils from "../../../../utils";
 import Button from "../../../UI/Button";
+import { openDirectory } from "../../../../redux/drive/DriveActions";
+import Path from "../../../../redux/drive/Path";
 
 export default function BackButton() {
   const dispatch = useDispatch();
 
   const label = "Open parent folder";
 
-  const currentGroupId = useSelector(
-    (state: AppState) => state.notes.currentGroupID
+  const cwd = useSelector(
+    (state: AppState) => state.drive.cwd
   );
 
-  const parentGroupId = useSelector((state: AppState) =>
-    utils.array.last(state.notes.groups[currentGroupId].path)
-  );
+  const parentDirectory = useSelector((state: AppState) => {
+    const dirname = Path.dirname(cwd);
+    if (state.drive.fs[dirname]) {
+      return dirname;
+    }
+
+    return undefined;
+  });
 
   function onClick() {
-    if (parentGroupId) {
-      dispatch(openGroup(parentGroupId));
+    if (parentDirectory) {
+      dispatch(openDirectory(parentDirectory));
     }
   }
 
@@ -31,7 +38,7 @@ export default function BackButton() {
       title={label}
       aria-label={label}
       onClick={onClick}
-      disabled={!Boolean(parentGroupId)}
+      disabled={!Boolean(parentDirectory)}
     >
       <MdArrowBack aria-hidden={true} />
     </Button>
