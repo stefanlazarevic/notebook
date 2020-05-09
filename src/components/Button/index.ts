13086a9e2403c42911ae66e0397c9fbfa19f5b1b
stefanlazarevic <stefanlazarevic.contact@gmail.com>
2020-05-09 20:32:47 +0200

    Added Help Button Import

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -3,9 +3,10 @@ import BackButton from "./components/BackButton";
 import CloseButton from "./components/CloseButton";
 import DownloadButton from "./components/DownloadButton";
 import EyeButton from "./components/EyeButton";
+import HelpButton from "./components/HelpButton";
 import NextButton from "./components/NextButton";
 import PreviousButton from "./components/PreviousButton";
 
-export { BackButton, CloseButton, DownloadButton, EyeButton, NextButton, PreviousButton };
+export { BackButton, CloseButton, DownloadButton, EyeButton, HelpButton, NextButton, PreviousButton };
 
 export default Button;
