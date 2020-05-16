8695ae0a5316fb26df4c42283964bb15d66028c8
stefanlazarevic <stefanlazarevic.contact@gmail.com>
2020-05-16 23:11:45 +0200

    Added more test cases for Checkbox.

--- src/components/Checkbox/__test__/DOM.test.tsx
+++ src/components/Checkbox/__test__/DOM.test.tsx
@@ -5,9 +5,7 @@ import Checkbox from "../index";
 afterEach(cleanup);
 
 it("Подразумевана вредност `tabIndex` атрибута је '0'.", () => {
-	const { getByTestId } = render(
-		<Checkbox testid="testid" onChange={() => undefined} />
-	);
+	const { getByTestId } = render(<Checkbox testid="testid" />);
 
 	const checkbox: HTMLElement = getByTestId("testid");
 
@@ -15,20 +13,34 @@ it("Подразумевана вредност `tabIndex` атрибута је
 	expect(checkbox.getAttribute("tabIndex")).toBe("0");
 });
 
-it("Онемогућено поље не поседује `tabIndex` атрибут.", () => {
-	const { getByTestId } = render(
-		<Checkbox testid="testid" disabled={true} onChange={() => undefined} />
-	);
+it("Онемогућено поље мора поседовати вредност `aria-disabled` атрибута `true`.", () => {
+	const { getByTestId } = render(<Checkbox testid="testid" disabled={true} />);
+
+	const checkbox: HTMLElement = getByTestId("testid");
+
+	expect(checkbox).toHaveAttribute("aria-disabled");
+	expect(checkbox.getAttribute("aria-disabled")).toBe("true");
+});
+
+it("Онемогућено поље не сме поседовати `tabIndex` атрибут.", () => {
+	const { getByTestId } = render(<Checkbox testid="testid" disabled={true} />);
 
 	expect(getByTestId("testid")).not.toHaveAttribute("tabIndex");
 });
 
 it("Подразумевана вредност `role` атрибута је 'checkbox'.", () => {
-	const { getByTestId } = render(
-		<Checkbox testid="testid" onChange={() => undefined} />
-	);
+	const { getByTestId } = render(<Checkbox testid="testid" />);
 
 	const checkbox: HTMLElement = getByTestId("testid");
 	expect(checkbox).toHaveAttribute("role");
 	expect(checkbox.getAttribute("role")).toBe("checkbox");
+});
+
+it("Атрибут `aria-checked` мора прихватати вредност `mixed`.", () => {
+	const { getByTestId } = render(<Checkbox testid="testid" checked="mixed" />);
+
+	const checkbox: HTMLElement = getByTestId("testid");
+
+	expect(checkbox).toHaveAttribute("aria-checked");
+	expect(checkbox.getAttribute("aria-checked")).toBe("mixed");
 });
