27076b480c14038da3e024c894e07f8ee8c17bdb
Stefan Lazarevic <stefanlazarevic.contact@gmail.com>
2020-05-25 19:12:18 +0200

    Refactored button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,14 +2,29 @@ import React from "react";
 
 import "./Button.css";
 
-import ButtonProps from "./ButtonProps";
+import { ButtonProps, ButtonPropTypes } from "./ButtonProps";
 
-export default function Button(props: ButtonProps) {
-  const className = props.className ? `Button ${props.className}` : "Button";
+import useClassNames from "../Utils/hooks/classNames";
 
-  return (
-    <button {...props} className={className}>
-      {props.children}
-    </button>
-  );
+function Button(props: ButtonProps) {
+	const className = useClassNames("Button", props.className);
+
+	return (
+		<button
+			id={props.id}
+			data-testid={props.testid}
+			className={className}
+			aria-label={props["aria-label"]}
+		>
+			{props.children}
+		</button>
+	);
 }
+
+Button.propTypes = ButtonPropTypes;
+
+Button.defaultProps = {};
+
+Button.displayName = "Button";
+
+export default Button;
--- src/components/Button/ButtonProps.ts
+++ src/components/Button/ButtonProps.ts
@@ -1,3 +1,26 @@
-import { ButtonHTMLAttributes } from "react";
+import PropTypes, { InferProps, Validator } from "prop-types";
 
-export default interface ButtonProps extends ButtonHTMLAttributes<any> {}
+export const ButtonPropTypes = {
+	/**
+	 *
+	 */
+	id: PropTypes.string as Validator<string | undefined>,
+	/**
+	 *
+	 */
+	testid: PropTypes.string as Validator<string | undefined>,
+	/**
+	 *
+	 */
+	className: PropTypes.string as Validator<string | undefined>,
+	/**
+	 *
+	 */
+	children: PropTypes.node as Validator<React.ReactNode | React.ReactNodeArray>,
+	/**
+	 *
+	 */
+	"aria-label": PropTypes.string as Validator<string | undefined>,
+};
+
+export type ButtonProps = InferProps<typeof ButtonPropTypes>;
