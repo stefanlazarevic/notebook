a4ab007bd7532c89a275b715e49f17392ef3d0d4
Stefan Lazarevic <stefanlazarevic.contact@gmail.com>
2020-05-29 19:10:25 +0200

    Improved Avatar image fallback text

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 import "./Avatar.css";
 
@@ -7,20 +7,35 @@ import useClassNames from "../Utils/hooks/classNames";
 function Avatar(props: any) {
 	const className = useClassNames("Avatar", props.className);
 
+	const [error, setError] = useState<boolean>(true);
+
+	const onImageLoad = useCallback(function AvatarImageLoadCallback() {
+		setError(false);
+	}, []);
+
 	return (
-		<img
+		<div
 			id={props.id}
 			data-testid={props.testid}
-			src={props.src}
-			alt={props.alt}
+			tabIndex={typeof props.tabIndex === "number" ? props.tabIndex : undefined}
 			className={className}
-		/>
+			aria-label={props["aria-label"]}
+		>
+			<img src={props.src} alt={props.alt} onLoad={onImageLoad} data-error={error} />
+			{error && (
+				<div className="AvatarInitials" aria-hidden={true}>
+					{props.name.charAt(0)}
+				</div>
+			)}
+		</div>
 	);
 }
 
 Avatar.propTypes = {};
 
-Avatar.defaultProps = {};
+Avatar.defaultProps = {
+	name: "Stefan",
+};
 
 Avatar.displayName = "Avatar";
 
